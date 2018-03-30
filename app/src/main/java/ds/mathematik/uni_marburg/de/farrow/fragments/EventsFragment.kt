96780352a7bfa13461814f195d8a6e0be3a37189
Thames1990 <ThomasMohr1990@gmail.com>
2018-03-30 17:55:46 +0200

    Use static map images for list view

--- app/src/main/java/ds/mathematik/uni_marburg/de/farrow/fragments/EventsFragment.kt
+++ app/src/main/java/ds/mathematik/uni_marburg/de/farrow/fragments/EventsFragment.kt
@@ -7,6 +7,12 @@ import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
+import com.mapbox.api.staticmap.v1.MapboxStaticMap
+import com.mapbox.api.staticmap.v1.StaticMapCriteria
+import com.mapbox.api.staticmap.v1.models.StaticMarkerAnnotation
+import com.mapbox.geojson.Point
+import com.squareup.picasso.Picasso
+import ds.mathematik.uni_marburg.de.farrow.BuildConfig
 import ds.mathematik.uni_marburg.de.farrow.R
 import ds.mathematik.uni_marburg.de.farrow.model.event.Event
 import ds.mathematik.uni_marburg.de.farrow.utils.inflate
@@ -47,10 +53,39 @@ class EventsFragment : BaseFragment() {
             override val containerView: View
         ) : RecyclerView.ViewHolder(containerView), LayoutContainer {
 
-            fun bindTo(item: Event?) {
-                if (item != null) {
-                    title_text.text = item.id.toString()
-                    subtitle_text.text = "${item.latitude}\n${item.longitude}"
+            fun bindTo(event: Event?) {
+                if (event != null) {
+                    val latitude: Double = event.latitude
+                    val longitude: Double = event.longitude
+
+                    title_text.text = event.id.toString()
+                    subtitle_text.text = "$latitude\n$longitude"
+
+                    val cameraPoint = Point.fromLngLat(latitude, longitude)
+
+                    val mapStaticImage = MapboxStaticMap
+                        .builder()
+                        .accessToken(BuildConfig.MAPBOX_KEY)
+                        .styleId(StaticMapCriteria.OUTDOORS_STYLE)
+                        .cameraPoint(cameraPoint)
+                        .cameraZoom(12.0)
+                        .staticMarkerAnnotations(
+                            listOf(
+                                StaticMarkerAnnotation.builder()
+                                    .lnglat(cameraPoint)
+                                    .build()
+                            )
+                        )
+                        .width(320)
+                        .height(180)
+                        .retina(true)
+                        .build()
+
+                    Picasso.get()
+                        .load(mapStaticImage.url().toString())
+                        .placeholder(R.color.cardview_dark_background)
+                        .error(android.R.color.holo_red_dark)
+                        .into(media_image)
                 }
             }
 
